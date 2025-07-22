5f99ff8e9035bb7d3ec8266d8baa8fd2b7211df9
Luke Zappia <luke@data-intuitive.com>
2025-07-22 08:50:05 +0200

    Add Open Problems helper functions

---
+++ src/czbenchmarks/openproblems/__init__.py
@@ -0,0 +1,7 @@
+from .openproblems import get_metric_executable, clone_task_repo, build_viash_component
+
+__all__ = [
+    "get_metric_executable",
+    "clone_task_repo",
+    "build_viash_component",
+]
