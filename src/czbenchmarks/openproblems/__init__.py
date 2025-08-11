a8a0da81614bc36cb2988aef8d23d3f4e0e71bb8
Luke Zappia <luke@data-intuitive.com>
2025-08-11 17:06:27 +0200

    Remove openproblems utils

--- src/czbenchmarks/openproblems/__init__.py
+++
@@ -1,7 +0,0 @@
-from .openproblems import get_metric_executable, clone_task_repo, build_viash_component
-
-__all__ = [
-    "get_metric_executable",
-    "clone_task_repo",
-    "build_viash_component",
-]
